da44af02809f8bcc07b3e2027347cb21e491f34e
Nilay Teja Kandula <131381875+nilaytejanani@users.noreply.github.com>
2024-12-23 01:28:12 +0530

    Create test.ps1
    test1

---
+++ test.ps1
@@ -0,0 +1,4 @@
+﻿##############################################################
+#  Run the Virtual Desktop Optimization Tool (VDOT)
+##############################################################
+# https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
